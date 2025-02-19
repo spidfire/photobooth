3cc8f9c483fa59008b781b16f192866ad2ff7fc8
Spidfire <spidfire@users.noreply.github.com>
2025-02-19 23:19:52 +0100

    Update main.js

--- main.js
+++ main.js
@@ -16,6 +16,7 @@ function createWindow () {
       app.quit(); // Exit the application when Escape is pressed
     }
   });
+  mainWindow.maximize();
   // and load the index.html of the app.
   mainWindow.loadFile('index.html')
 
